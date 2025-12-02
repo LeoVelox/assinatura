e6beb1a802357b92bc8c1c9eea46b8cae3eef0a7
Leonardo Silvio Sarmento <leo1.silvio@gmail.com>
2025-12-02 12:08:01 -0300

    Atualiza cadastro-pagamento.js

--- assets/js/cadastro-pagamento.js
+++ assets/js/cadastro-pagamento.js
@@ -245,7 +245,6 @@ async function createTrialAccount() {
 
     // 3. Criar usuário no Supabase Auth
     console.log("Criando usuário no Auth...");
-    console.log("🔧 Client criado:", !!client);
     const { data: authResult, error: signUpError } = await supabase.auth.signUp(
       {
         email: userData.email,
